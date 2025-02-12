c9e5a38539ce1af81bb67b3b2cf69eee8860930d
trucdtd <duongdieutruc09@gmail.com>
2025-02-13 00:40:26 +0700

    .

--- obj/Debug/net6.0/ISC_ELIB_SERVER.AssemblyInfo.cs
+++ obj/Debug/net6.0/ISC_ELIB_SERVER.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISC_ELIB_SERVER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577240cf1d49a4bfb4d24f49c0040afc61fbdcc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86691239db5c6ce32f105af41a1b7d7aaf54df62")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISC_ELIB_SERVER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISC_ELIB_SERVER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
