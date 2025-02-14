3cdbd54aab96870e2c33beb5b35e9cc414ecfb50
PhanDungNho <dungnho.dev@gmail.com>
2025-02-14 16:02:16 +0700

    CRUD TestsAttachment, TestsSumissionsAnswer, Exams

---
+++ Repositories/TestAnswerRepo.cs
@@ -0,0 +1,18 @@
+﻿using ISC_ELIB_SERVER.Models;
+
+namespace ISC_ELIB_SERVER.Repositories
+{
+    public class TestAnswerRepo
+    {
+        private readonly isc_elibContext _context;
+        public TestAnswerRepo(isc_elibContext context)
+        {
+            _context = context;
+        }
+
+        public bool IsCorrectAnswer(long answerId)
+        {
+            return _context.TestAnswers.Any(a => a.Id == answerId && a.IsCorrect == true);
+        }
+    }
+}
---
+++ Repositories/TestSubmissionAnswerRepo.cs
@@ -0,0 +1,43 @@
+﻿using ISC_ELIB_SERVER.Models;
+
+namespace ISC_ELIB_SERVER.Repositories
+{
+    public class TestSubmissionAnswerRepo
+    {
+        private readonly isc_elibContext _context;
+        public TestSubmissionAnswerRepo(isc_elibContext context)
+        {
+            _context = context;
+        }
+        public ICollection<TestSubmissionsAnswer> GetAllTestSubmissionAnswers()
+        {
+            return _context.TestSubmissionsAnswers.ToList();
+        }
+        public TestSubmissionsAnswer GetTestSubmissionAnswerById(long id)
+        {
+            return _context.TestSubmissionsAnswers.FirstOrDefault(s => s.Id == id);
+        }
+        public TestSubmissionsAnswer CreateTestSubmissionAnswer(TestSubmissionsAnswer testSubmissionAnswer)
+        {
+            _context.TestSubmissionsAnswers.Add(testSubmissionAnswer);
+            _context.SaveChanges();
+            return testSubmissionAnswer;
+        }
+        public TestSubmissionsAnswer UpdateTestSubmissionAnswer(TestSubmissionsAnswer entity)
+        {
+            _context.TestSubmissionsAnswers.Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+        public bool DeleteTestSubmissionAnswer(long id)
+        {
+            var testSubmissionAnswer = GetTestSubmissionAnswerById(id);
+            if (testSubmissionAnswer != null)
+            {
+                _context.TestSubmissionsAnswers.Remove(testSubmissionAnswer);
+                return _context.SaveChanges() > 0;
+            }
+            return false;
+        }
+    }
+}
