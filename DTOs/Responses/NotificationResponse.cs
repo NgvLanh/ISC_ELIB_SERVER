1d4de8d7cf525298f3fa4575befd685cc49ef1d8
Đinh Văn Phát <145641361+DinhVanPhat@users.noreply.github.com>
2025-02-14 13:57:32 +0700

    update gruop-3-vphat

---
+++ DTOs/Responses/NotificationResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace ISC_ELIB_SERVER.DTOs.Responses
+{
+    public class NotificationResponse
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public long SenderId { get; set; }
+        public long UserId { get; set; }
+    }
+}
