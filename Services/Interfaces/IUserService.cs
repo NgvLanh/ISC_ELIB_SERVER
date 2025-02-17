ddc9b4796f7e0fa84a9cdb2329822c6c069a859e
trucdtd <duongdieutruc09@gmail.com>
2025-02-17 22:34:51 +0700

    update Interface
    IUserService, IStudentInfoService, ITeacherInfoService

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using ISC_ELIB_SERVER.DTOs.Requests;
+using ISC_ELIB_SERVER.DTOs.Responses;
+using ISC_ELIB_SERVER.Models;
+
+namespace ISC_ELIB_SERVER.Services.Interfaces
+{
+    public interface IUserService
+    {
+        ApiResponse<ICollection<UserResponse>> GetUsers(int page, int pageSize, string search, string sortColumn, string sortOrder);
+        ApiResponse<UserResponse> GetUserById(long id);
+        ApiResponse<UserResponse> GetUserByCode(string code);
+        ApiResponse<UserResponse> CreateUser(UserRequest userRequest);
+        ApiResponse<UserResponse> UpdateUser(UserRequest userRequest);
+        ApiResponse<User> DeleteUser(long id);
+    }
+}
