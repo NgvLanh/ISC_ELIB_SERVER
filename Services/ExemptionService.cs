8e668c4225f757f95a3d60d058b9d08acbd7ad76
phuoctm1910 <nothinginthis465@gmail.com>
2025-02-20 10:43:28 +0700

    Fix Service
    (cherry picked from commit d12d01776cb1977ee4c8ecdd0c159b8b5dd70118)

--- Services/ExemptionService.cs
+++ Services/ExemptionService.cs
@@ -3,18 +3,10 @@ using ISC_ELIB_SERVER.Repositories;
 using ISC_ELIB_SERVER.DTOs.Responses;
 using ISC_ELIB_SERVER.DTOs.Requests;
 using AutoMapper;
+using ISC_ELIB_SERVER.Services.Interfaces;
 
 namespace ISC_ELIB_SERVER.Services
 {
-    public interface IExemptionService
-    {
-        ApiResponse<ICollection<ExemptionResponse>> GetExemptions(int page, int pageSize, string search, string sortColumn, string sortOrder);
-        ApiResponse<ICollection<ExemptionResponse>> GetExemptionsNormal();
-        ApiResponse<ExemptionResponse> GetExemptionById(long id);
-        ApiResponse<ExemptionResponse> CreateExemption(Exemption_AddRequest ExemptionRequest);
-        ApiResponse<Exemption> UpdateExemption(long id,Exemption_UpdateRequest Exemption);
-        ApiResponse<Exemption> DeleteExemption(long id);
-    }
 
 
     public class ExemptionService : IExemptionService
