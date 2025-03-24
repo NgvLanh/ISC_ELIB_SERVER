f1a877ebc534e7a3db163ff966d56ab92fec3523
trucdtd <duongdieutruc09@gmail.com>
2025-03-24 16:00:18 +0700

    update ReserveListResponse
    Thêm DS ReserveList theo figma

---
+++ DTOs/Responses/ReserveListResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ISC_ELIB_SERVER.DTOs.Responses
+{
+    public class ReserveListResponse
+    {
+        public int StudentId { get; set; }  // Student ID (Mã hồ sơ bảo lưu)
+        public string? FullName { get; set; }  // Tên đầy đủ từ bảng User
+        public DateTime? Dob { get; set; }  // Ngày sinh từ bảng User
+        public string? Gender { get; set; }  // Giới tính từ bảng User
+        public string? ClassName { get; set; } // Tên lớp từ bảng Class
+        public DateTime? ReserveDate { get; set; }  // Ngày bảo lưu
+        public string? RetentionPeriod { get; set; }  // Thời gian bảo lưu
+        public string? Reason { get; set; }  // Lý do
+    }
+}
