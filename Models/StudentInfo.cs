9b3f9712f25f74a1dc83d904caf284cceeb5033e
DangLH <ledang01637@gmail.com>
2025-04-03 23:06:02 +0700

    fix create studentinfo datetime

--- Models/StudentInfo.cs
+++ Models/StudentInfo.cs
@@ -9,7 +9,7 @@ namespace ISC_ELIB_SERVER.Models
         public string? GuardianName { get; set; }
         public string? GuardianPhone { get; set; }
         public string? GuardianJob { get; set; }
-        public DateTime? GuardianDob { get; set; }
+        public DateTime GuardianDob { get; set; }
         public string? GuardianAddress { get; set; }
         public string? GuardianRole { get; set; }
         public int? UserId { get; set; }
