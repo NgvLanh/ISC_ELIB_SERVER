848968184397692f3243e17ead24cd53c52699ad
trucdtd <duongdieutruc09@gmail.com>
2025-04-06 14:53:01 +0700

    update API api/studentinfos/all
    Cập nhật lại Id theo bảng Users

--- Mappers/StudentInfoMapper.cs
+++ Mappers/StudentInfoMapper.cs
@@ -17,25 +17,26 @@ namespace ISC_ELIB_SERVER.Mappers
 
             // Mapping từ StudentInfo -> StudentInfoUserResponse (lấy thêm thông tin từ User, Class, UserStatus)
             CreateMap<StudentInfo, StudentInfoUserResponse>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User != null ? src.User.Id : (int?)null))
                 .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.User != null ? src.User.Code : null))
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.User != null ? src.User.FullName : null))
-                .ForMember(dest => dest.Dob, opt => opt.MapFrom(src => src.User != null ? src.User.Dob : null))
+                .ForMember(dest => dest.Dob, opt => opt.MapFrom(src => src.User != null ? src.User.Dob : (DateTime?)null))
                 .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.User != null ? (src.User.Gender == true ? "Nam" : "Nữ") : null))
                 .ForMember(dest => dest.Nation, opt => opt.MapFrom(src => src.User != null ? src.User.Nation : null))
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.User != null && src.User.Class != null ? src.User.Class.Name : null))
                 .ForMember(dest => dest.status, opt => opt.MapFrom(src => src.User != null && src.User.UserStatus != null ? src.User.UserStatus.Name : null))
-            // Lấy thông tin AcademicYearId và Semesters từ User -> AcademicYear
-            .ForMember(dest => dest.AcademicYearId, opt => opt.MapFrom(src => src.User != null && src.User.AcademicYear != null ? src.User.AcademicYear.Id : (int?)null))
-            .ForMember(dest => dest.Semesters, opt => opt.MapFrom(src => src.User != null && src.User.AcademicYear != null ?
-                src.User.AcademicYear.Semesters.Select(se => new SemesterResponse
-                {
-                    Id = se.Id,
-                    Name = se.Name,
-                    StartTime = se.StartTime ?? default(DateTime),
-                    EndTime = se.EndTime ?? default(DateTime),
-                    AcademicYearId = se.AcademicYearId.HasValue ? se.AcademicYearId.Value : 0
-                }).ToList() : new List<SemesterResponse>())
-            );
+                // Lấy thông tin AcademicYearId và Semesters từ User -> AcademicYear
+                .ForMember(dest => dest.AcademicYearId, opt => opt.MapFrom(src => src.User != null && src.User.AcademicYear != null ? src.User.AcademicYear.Id : (int?)null))
+                .ForMember(dest => dest.Semesters, opt => opt.MapFrom(src => src.User != null && src.User.AcademicYear != null ?
+                    src.User.AcademicYear.Semesters.Select(se => new SemesterResponse
+                    {
+                        Id = se.Id,
+                        Name = se.Name,
+                        StartTime = se.StartTime ?? default(DateTime),
+                        EndTime = se.EndTime ?? default(DateTime),
+                        AcademicYearId = se.AcademicYearId.HasValue ? se.AcademicYearId.Value : 0
+                    }).ToList() : new List<SemesterResponse>())
+                );
 
             // Mapping từ StudentInfo -> StudentInfoClassResponse (lấy thêm thông tin từ User, AcademicYear, UserStatus)
             CreateMap<StudentInfo, StudentInfoClassResponse>()
@@ -47,7 +48,7 @@ namespace ISC_ELIB_SERVER.Mappers
                         ? src.User.AcademicYear.StartTime.Value.Year + "-" + src.User.AcademicYear.EndTime.Value.Year
                         : null
                     : null))
-                .ForMember(dest => dest.EnrollmentDate, opt => opt.MapFrom(src => src.User != null ? src.User.EnrollmentDate : null))
+                .ForMember(dest => dest.EnrollmentDate, opt => opt.MapFrom(src => src.User != null ? src.User.EnrollmentDate : (DateTime?)null))
                 .ForMember(dest => dest.UserStatusName, opt => opt.MapFrom(src => src.User != null && src.User.UserStatus != null ? src.User.UserStatus.Name : null));
         }
     }
