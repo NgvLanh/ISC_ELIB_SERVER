b3ba341aaa4caf65fb342049ddc1336c3a44619e
Haktnpc06569 <145770435+Haktnpc06569@users.noreply.github.com>
2025-04-08 21:16:43 +0700

    gr2_crudScore

--- DTOs/Responses/StudentScoreResponse.cs
+++ DTOs/Responses/StudentScoreResponse.cs
@@ -8,10 +8,10 @@ namespace ISC_ELIB_SERVER.DTOs.Responses
     {
         public int Id { get; set; }
         public double? Score { get; set; }
-        public int ScoreTypeId { get; set; }
-        public int SubjectId { get; set; }
-        public int UserId { get; set; }
-        public int SemesterId { get; set; }
+        public TypeScoreResponse ScoreType { get; set; }
+        public SubjectScoreResponse Subject { get; set; }
+        public StudentResponse Student { get; set; }
+        public SemesterScoreResponse Semester { get; set; }
     }
 
     public class StudentScoreDashboardResponse
